11081bbc41e8fa3acc84371c9043c6291661cd60
Nat Welch <nat@natwelch.com>
2018-12-26 06:15:32 +0000

    Generate probe config

---
+++ make-probe-file.rb
@@ -0,0 +1,38 @@
+#! /usr/bin/env ruby
+
+open('probe.cfg', 'w') do |f|
+  File.readlines('hosts.txt').each do |line|
+    domain = line.strip
+    name = "#{domain.gsub(/\./, "_")}"
+
+    template = %{
+probe {
+  name: "#{name}_homepage"
+  type: HTTPS
+  targets {
+    host_names: "#{domain}"
+  }
+  interval_msec: 5000  # 5s
+  timeout_msec: 1000   # 1s
+}
+
+probe {
+  name: "#{name}_healthz"
+  type: HTTP
+  targets {
+    host_names: "#{domain}"
+  }
+
+  http_probe {
+    relative_url: "/healthz"
+  }
+
+  interval_msec: 5000  # 5s
+  timeout_msec: 1000   # 1s
+}
+    }.strip
+
+    f.puts template
+    f.puts "\n"
+  end
+end
